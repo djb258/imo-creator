f3f749b4f7df2d9841336d06c5a9e3180103d2a0
Dave Barton <dbarton@svg.agency>
2025-08-21 12:24:48 -0400

    Remove conflicting JavaScript API files - using Python only

--- api/hello.js
+++
@@ -1,7 +0,0 @@
-export default function handler(request, response) {
-  response.status(200).json({
-    message: 'Hello from Vercel API!',
-    url: request.url,
-    method: request.method
-  });
-}
